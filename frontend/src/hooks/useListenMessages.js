b765f0f59d3928a5dc9c997270487ad8547aa01b
Vinay Pandey <pandeyjivinay8765@gmail.com>
2026-03-10 19:40:30 +0530

    Refactor message state update in useListenMessages
    Updated message handling to use functional state update for setMessages.

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-
 import { useSocketContext } from "../context/SocketContext";
 import useConversation from "../zustand/useConversation";
 
@@ -7,17 +6,23 @@ import notificationSound from "../assets/sounds/notification.mp3";
 
 const useListenMessages = () => {
 	const { socket } = useSocketContext();
-	const { messages, setMessages } = useConversation();
+	const { setMessages } = useConversation();
 
 	useEffect(() => {
 		socket?.on("newMessage", (newMessage) => {
 			newMessage.shouldShake = true;
+
+			// play notification sound
 			const sound = new Audio(notificationSound);
 			sound.play();
-			setMessages([...messages, newMessage]);
+
+			// FIX: functional state update
+			setMessages((prevMessages) => [...prevMessages, newMessage]);
 		});
 
+		// cleanup listener
 		return () => socket?.off("newMessage");
-	}, [socket, setMessages, messages]);
+	}, [socket, setMessages]);
 };
+
 export default useListenMessages;
